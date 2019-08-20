e3ca540043aaebafe964bdd70b3e36da907c2441
zhong <zhongxuebin171980062@gmail.com>
2019-08-20 22:15:03 +0800

    SpringBoot WebMvc的实现 (二)

--- springboot-webmvc/src/main/java/com/zxb/web/config/WebMvcConfig.java
+++ springboot-webmvc/src/main/java/com/zxb/web/config/WebMvcConfig.java
@@ -19,21 +19,23 @@ import javax.servlet.http.HttpServletResponse;
  * @date 2019-08-20 09:31
  */
 @Configuration  // 注解配置，Spring范式注解
-@EnableWebMvc   // 组件激活，Spring模块装配
+// @EnableWebMvc   // 组件激活，Spring模块装配    //该注解在SpringFramework WebMVC自动装配时和SpringBoot WebMVC自动装配有差异
+// 因为 WebMvcAutoConfiguration类的互斥条件影响，当@ConditionalOnMissingBean(WebMvcConfigurationSupport.class)
+// 没有配置WebMVC时才会自动化配置
 public class WebMvcConfig implements WebMvcConfigurer {
 
     /**
-     * 配置视图解析器
+     * 配置视图解析器，硬编码方式，通过application.properties配置优化该方式
      * @return
      */
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/jsp/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
+//    @Bean
+//    public ViewResolver viewResolver() {
+//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//        viewResolver.setViewClass(JstlView.class);
+//        viewResolver.setPrefix("/WEB-INF/jsp/");
+//        viewResolver.setSuffix(".jsp");
+//        return viewResolver;
+//    }
 
     /**
      * 添加拦截器
