b6f3cf040347b41fb839ffda0c34f6c4fa80d744
zhongxb <zxb@sengled.com>
2019-08-27 17:40:32 +0800

    SpringBoot嵌入Servlet容器

---
+++ spring-boot-servlet/src/main/java/com/zxb/spring/boot/web/servlet/SpringBootServletBootstrap.java
@@ -0,0 +1,46 @@
+package com.zxb.spring.boot.web.servlet;
+
+import com.zxb.spring.web.servlet.AsyncServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
+
+/**
+ * Spring Boot Servlet 引导类
+ * @author Mr.zxb
+ * @date 2019-08-27 16:29
+ */
+@EnableAutoConfiguration
+// Servlet 组件扫描
+//@ServletComponentScan(basePackages = "com.zxb.spring.web.servlet")
+public class SpringBootServletBootstrap {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootServletBootstrap.class, args);
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public AsyncServlet asyncServlet() {
+        return new AsyncServlet();
+    }
+
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        return servletContext -> {
+            CharacterEncodingFilter filter = new CharacterEncodingFilter();
+            FilterRegistration.Dynamic registration = servletContext.addFilter("filter", filter);
+            registration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/");
+        };
+    }
+}
