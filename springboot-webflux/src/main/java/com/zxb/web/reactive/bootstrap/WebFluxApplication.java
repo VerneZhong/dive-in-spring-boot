8bf8ce513f2c038edecdc432543465f796f5e6a8
zhongxb <zxb@sengled.com>
2019-09-03 10:38:23 +0800

    WebFlux之映射函数式接口编程

---
+++ springboot-webflux/src/main/java/com/zxb/web/reactive/bootstrap/WebFluxApplication.java
@@ -0,0 +1,56 @@
+package com.zxb.web.reactive.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+/**
+ * @author Mr.zxb
+ * @date 2019-09-03 09:13
+ */
+@SpringBootApplication
+public class WebFluxApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WebFluxApplication.class, args);
+    }
+
+    /**
+     * 映射路由接口- RouterFunction
+     * 函数式接口映射
+     * @return
+     */
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction() {
+        // 方式一
+//        return RouterFunctions.route(request -> {
+//            // 判断请求是否匹配
+//            URI uri = request.uri();
+//            return "/route".equals(uri.getPath()) && request.method().matches("GET");
+//        }, request -> {
+//            // 绑定实现处理
+//            return ServerResponse.status(HttpStatus.OK)
+//                    .body(Mono.just("hello webflux."), String.class);
+//        });
+
+        // 方式二
+        return route(GET("/route"), this::routeMethod);
+    }
+
+    public Mono<ServerResponse> routeMethod(ServerRequest serverRequest) {
+        return ServerResponse.status(HttpStatus.OK)
+                    .body(Mono.just("hello webflux."), String.class);
+    }
+}
